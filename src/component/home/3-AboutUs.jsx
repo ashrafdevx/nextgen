6440749fc5d53805297da1eb9e2f73b72bb14013
ashrafbangash <ashraf.devx@gmail.com>
2024-12-31 00:23:09 +0500

    Image are updaed

--- src/component/home/3-AboutUs.jsx
+++ src/component/home/3-AboutUs.jsx
@@ -1,10 +1,10 @@
 const AboutUs = () => {
   return (
     <>
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-center md:justify-between px-16 bg-white  md:px-16 py-10 md:py-16">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center px-8 md:px-16 bg-white py-10 md:py-8">
         {/* Left Section */}
         <div className="max-w-xl text-center md:text-left">
-          <button className="border  transition duration-300 border-black hover:border-black px-8 py-2 rounded-full text-md mb-5 hover:bg-black hover:text-white ease-in-out transition delay-75">
+          <button className="border  transition duration-300 border-black hover:border-black px-8 py-2 rounded-full text-md mb-5 hover:bg-black hover:text-white ease-in-out delay-75">
             About Us
           </button>
           <div>
@@ -35,34 +35,34 @@ const AboutUs = () => {
           <img
             src="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a568f31e8a98567c19008_pexels-rdne-8292895.jpg"
             alt="Real Estate"
-            className=" shadow-lg max-w-full "
+            className="md:h-[500px] h-300 shadow-lg max-w-full object-cover"
           />
         </div>
       </div>
 
       {/* Build for Individual HomeTown Div */}
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-center md:justify-between px-16 bg-white  md:px-16 py-10 md:py-8">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center px-8 md:px-16 bg-white py-10 md:py-8">
         {/* Right Section */}
-        <div className="mt-8 md:mt-0 max-w-2xl">
+        <div className="max-w-full">
           <img
             src="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a56fa59f4cc3345d4cd60_pexels-dropshado-2251247.jpg"
             alt="Real Estate"
-            className=" shadow-lg max-w-full "
+            className="md:h-[500px] h-300 shadow-lg max-w-full object-cover"
           />
         </div>
+
         {/* Left Section */}
         <div className="max-w-xl text-center md:text-left">
-          <button className="border  duration-300 border-black hover:border-black px-8 py-2 rounded-full text-md mb-5 hover:bg-black hover:text-white ease-in-out transition delay-75">
+          <button className="border duration-300 border-black hover:border-black px-8 py-2 rounded-full text-md mb-5 hover:bg-black hover:text-white transition ease-in-out delay-75">
             Service
           </button>
           <div>
-            <h1 className="text-3xl md:text-5xl md:py-5 font-bold leading-tight ">
+            <h1 className="text-3xl md:text-5xl md:py-5 font-bold leading-tight">
               Build for Individuals & Homeowners
             </h1>
-            {/* <h1 className="text-6xl py-5 pt-0">Investments</h1> */}
           </div>
           <p className="text-gray-600 mb-6">
-            At NextGen Properties , we bring your dream home to life with expert
+            At NextGen Properties, we bring your dream home to life with expert
             craftsmanship and personalized designs. From concept to completion,
             our dedicated team ensures every detail reflects your unique vision,
             delivering a home that combines quality, style, and lasting value.
@@ -71,7 +71,7 @@ const AboutUs = () => {
             <button className="bg-blue-600 text-white transition duration-300 px-9 py-2 rounded-full shadow-md hover:bg-blue-700">
               Consult Now
             </button>
-            <button className="border transition duration-300 border-blue-600 text-blue-600 hover:bg-blue-900 py-2 px-9  rounded-full hover:text-white">
+            <button className="border transition duration-300 border-blue-600 text-blue-600 hover:bg-blue-900 py-2 px-9 rounded-full hover:text-white">
               Read More
             </button>
           </div>
@@ -79,7 +79,7 @@ const AboutUs = () => {
       </div>
 
       {/* Build for Investment and Manager */}
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-center md:justify-between px-16 bg-white  md:px-16 py-10 md:py-16">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center px-8 md:px-16 bg-white py-10 md:py-8">
         {/* Left Section */}
         <div className="max-w-xl text-center md:text-left">
           <div>
@@ -108,20 +108,20 @@ const AboutUs = () => {
           <img
             src="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a57606194fcd30b191155_pexels-enric-cruz-lopez-6039188.jpg"
             alt="Real Estate"
-            className=" shadow-lg max-w-full "
+            className="md:h-[500px] h-300 shadow-lg max-w-full object-cover"
           />
         </div>
       </div>
 
       {/* Real State Investment Manangement */}
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-center md:justify-between px-16 bg-white  md:px-16 py-10 md:py-16">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center px-8 md:px-16 bg-white py-10 md:py-8">
         {/* Right Section */}
         <div className="mt-8 md:mt-0 max-w-2xl">
           <img
             // src="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a568f31e8a98567c19008_pexels-rdne-8292895.jpg"
             src="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a575f050f09d0cde7a510_pexels-vividcafe-681333.jpg"
             alt="Real Estate"
-            className=" shadow-lg max-w-full "
+            className="md:h-[500px] h-300 shadow-lg max-w-full object-cover"
           />
         </div>
 
