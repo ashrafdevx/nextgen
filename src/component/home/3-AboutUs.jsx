65eeb9586237ce99d137bf819297c03cee808ceb
ashrafnasib <ashraf.devx@gmail.com>
2024-12-31 16:00:59 +0500

    Image Overlay and preview added

--- src/component/home/3-AboutUs.jsx
+++ src/component/home/3-AboutUs.jsx
@@ -1,4 +1,8 @@
+// import { X, Expand } from "lucide-react";
+// import { useState } from "react";
+import ImageWithOverlay from "../../utils/ImagePreview";
 const AboutUs = () => {
+  // const [isPreviewOpen, setIsPreviewOpen] = useState(false);
   return (
     <>
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center px-8 md:px-16 bg-white py-10 md:py-8">
@@ -31,24 +35,55 @@ const AboutUs = () => {
         </div>
 
         {/* Right Section */}
-        <div className="mt-8 md:mt-0 max-w-2xl">
-          <img
-            src="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a568f31e8a98567c19008_pexels-rdne-8292895.jpg"
-            alt="Real Estate"
-            className="md:h-[500px] h-300 shadow-lg max-w-full object-cover"
-          />
-        </div>
+        <ImageWithOverlay img="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a568f31e8a98567c19008_pexels-rdne-8292895.jpg" />
+        {/* <>
+          <div className="mt-8 md:mt-0 max-w-2xl relative group">
+            <img
+              src="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a568f31e8a98567c19008_pexels-rdne-8292895.jpg"
+              alt="Real Estate"
+              className="md:h-[500px] h-300 shadow-lg max-w-full object-cover"
+            />
+            <div
+              className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center cursor-pointer"
+              onClick={() => setIsPreviewOpen(true)}
+            >
+              <span className="text-white text-xl font-semibold">
+                <Expand size={48} />
+              </span>
+            </div>
+          </div>
+
+          {/* Preview Modal 
+          {isPreviewOpen && (
+            <div className="fixed inset-0 bg-black/80 z-50 flex items-center justify-center p-4">
+              <div className="relative max-w-7xl w-full">
+                <button
+                  onClick={() => setIsPreviewOpen(false)}
+                  className="absolute -top-12 right-0 text-white hover:text-gray-300"
+                >
+                  <X size={32} />
+                </button>
+                <img
+                  src="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a568f31e8a98567c19008_pexels-rdne-8292895.jpg"
+                  alt="Real Estate Preview"
+                  className="w-full h-auto max-h-[90vh] object-contain"
+                />
+              </div>
+            </div>
+          )}
+        </> */}
       </div>
 
       {/* Build for Individual HomeTown Div */}
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center px-8 md:px-16 bg-white py-10 md:py-8">
         {/* Right Section */}
         <div className="max-w-full">
-          <img
+          <ImageWithOverlay img="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a56fa59f4cc3345d4cd60_pexels-dropshado-2251247.jpg" />
+          {/* <img
             src="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a56fa59f4cc3345d4cd60_pexels-dropshado-2251247.jpg"
             alt="Real Estate"
             className="md:h-[500px] h-300 shadow-lg max-w-full object-cover"
-          />
+          /> */}
         </div>
 
         {/* Left Section */}
@@ -105,11 +140,12 @@ const AboutUs = () => {
 
         {/* Right Section */}
         <div className="mt-8 md:mt-0 max-w-2xl">
-          <img
+          <ImageWithOverlay img="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a57606194fcd30b191155_pexels-enric-cruz-lopez-6039188.jpg" />
+          {/* <img
             src="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a57606194fcd30b191155_pexels-enric-cruz-lopez-6039188.jpg"
             alt="Real Estate"
             className="md:h-[500px] h-300 shadow-lg max-w-full object-cover"
-          />
+          /> */}
         </div>
       </div>
 
@@ -117,12 +153,7 @@ const AboutUs = () => {
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center px-8 md:px-16 bg-white py-10 md:py-8">
         {/* Right Section */}
         <div className="mt-8 md:mt-0 max-w-2xl">
-          <img
-            // src="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a568f31e8a98567c19008_pexels-rdne-8292895.jpg"
-            src="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a575f050f09d0cde7a510_pexels-vividcafe-681333.jpg"
-            alt="Real Estate"
-            className="md:h-[500px] h-300 shadow-lg max-w-full object-cover"
-          />
+          <ImageWithOverlay img="https://cdn.prod.website-files.com/6703f76c902df755b27afd5c/671a575f050f09d0cde7a510_pexels-vividcafe-681333.jpg" />
         </div>
 
         {/* Left Section */}
