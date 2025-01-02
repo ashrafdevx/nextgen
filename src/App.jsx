c8c6d324b4eaa6b91fb9410a3906e813f6fb9194
ashrafnasib <ashraf.devx@gmail.com>
2025-01-02 18:40:10 +0500

    investors-fund-manager added

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import PrivacyPolicy from "./pages/privacyPolicy";
 import Blogs from "./pages/blogs";
 import Header from "./component/header";
 import Footer from "./component/footer";
-import InvesterAndFundManagers from "./pages/services/InvesterAndFundManagers/InvesterAndFundManagers";
 import EstateInvestmentAndManagement from "./pages/services/estateInvesterManagement/EstateInvestmentAndManagement";
 import Services from "./pages/services/services";
 import IndividualAndHomeOwner from "./pages/services/individualAndHomeOwner/index";
+import InvesterAndFundManagers from "./pages/services/InvesterAndFundManagers";
 
 function App() {
   return (
