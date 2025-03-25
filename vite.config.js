93bf736cbc50d9dbf769d89f81551fc03af64244
ashrafbangash <ashraf.devx@gmail.com>
2025-03-26 01:44:53 +0500

    added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 import { imagetools } from "vite-imagetools";
 
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     imagetools(), // Add the imagetools plugin
+//   ],
+// });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [
-    react(),
-    imagetools(), // Add the imagetools plugin
-  ],
+  plugins: [react(), imagetools()],
+  server: {
+    historyApiFallback: true, // optional for local dev
+  },
+  build: {
+    outDir: "dist",
+  },
 });
